40dd97522610d5fd50514ee7aea531774fc10bd6
terryyin <terry@odd-e.com>
2013-01-29 21:05:34 +0800

    add backlog and makefile

---
+++ ProductBacklog.md
@@ -0,0 +1,5 @@
+Product Backlog
+===========================================
+Verify text successfully
+report mismatch error
+raise missing approval error if has not been verified yet
---
+++ test/AprovalTestsTest.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 29, 2013
+
+@author: yinterry
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
