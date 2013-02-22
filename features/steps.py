58c174e8b67e11d7b820dd33fdf229ea6ef9982b
Daniel Teng <tengzhenyu@gmail.com>
2013-02-22 12:06:22 +0800

    refactoring - has filemanager class and move test methods to test class

--- features/steps.py
+++ features/steps.py
@@ -8,51 +8,68 @@ class ApprovalError(Exception):
 	
 	def __str__(self):
 		return repr(self.value)
-		
-class Approvals:
-	approved_string = ""
 
-	@staticmethod
-	def init_verified_string():	
-		try:
-			remove("test.approved.txt")
-			remove("test.received.txt")
-		except OSError:
-			print("ok")
+class FileManager():
 		
-		f = file("test.approved.txt",'w')
-		f.write("hello world")
+	def load_approved_string(self):
+		f = file("test.approved.txt",'r')
+		approved_string = f.read()
 		f.close()
-		
+		return approved_string
 	
-	@staticmethod	
-	def init_string_not_verified():	
-		try:
-			remove("test.approved.txt")
-			remove("test.received.txt")
-		except OSError:
-			print("ok")
-						
-		f = file("test.approved.txt",'w')
-		f.write("hello")
-		f.close()
-		
-	@staticmethod
-	def save_file(text):
+	def save_approved_file(self,text):
 		f = file("test.received.txt",'w')
 		f.write(text)
 		f.close()
-	
+
+		
+class Approvals:
+	approved_string = ""
+	file_manager = FileManager()
+		
 	@staticmethod
 	def verify(text):
-		f = file("test.approved.txt",'r')
-		Approvals.approved_string = f.read()
-		f.close()
+		Approvals.approved_string = Approvals.file_manager.load_approved_string()
 		
 		if Approvals.approved_string != text:
-			Approvals.save_file(text)			
+			Approvals.file_manager.save_approved_file(text)			
 			raise ApprovalError("not approved yet")
+
+class ApprovalsForTest(Approvals):
+	@staticmethod
+	def init_verified_string():	
+		ApprovalsForTest.cleanup_test_files()
+		ApprovalsForTest.init_verified_file()
+
+	@staticmethod
+	def cleanup_test_files():
+		try:
+			remove("test.approved.txt")
+			remove("test.received.txt")
+		except OSError:
+			print("ok")
+
+	@staticmethod
+	def init_verified_file():
+		approved_text = "hello world"
+		ApprovalsForTest.init_approved_file(approved_text)
+
+	@staticmethod
+	def init_not_verified_file():
+		not_approved_text = "not approved"
+		ApprovalsForTest.init_approved_file(not_approved_text)
 		
+	@staticmethod
+	def init_approved_file(text):
+		f = file("test.approved.txt",'w')
+		f.write(text)
+		f.close()
+		
+	@staticmethod	
+	def init_string_not_verified():	
+		ApprovalsForTest.cleanup_test_files()
+		ApprovalsForTest.init_not_verified_file()
+			
 @step(u'When I verify "([^"]*)"')
 def when_i_verify(step, a_string):
 	world.a_string = a_string
@@ -60,7 +77,7 @@ def when_i_verify(step, a_string):
 @step(u'Then I will get an error')
 @raises(ApprovalError)
 def then_i_will_get_an_error(step):
-	Approvals.verify(world.a_string)
+	ApprovalsForTest.verify(world.a_string)
 	
 @step(u'And I get a received file')
 def and_i_get_a_received_file(step):
@@ -68,15 +85,15 @@ def and_i_get_a_received_file(step):
 
 @step(u'Given "([^"]*)" is verified')
 def given_a_string_is_verified(step, a_string):
-	Approvals.init_verified_string()
+	ApprovalsForTest.init_verified_string()
 
 @step(u'Given "([^"]*)" is not verified')
 def given_a_string_is_not_verified(step, a_string):
-    Approvals.init_string_not_verified()
+    ApprovalsForTest.init_string_not_verified()
 	
 @step(u'Then There is no error')
 def then_there_is_no_error(step):
-	Approvals.verify(world.a_string)
+	ApprovalsForTest.verify(world.a_string)
 	
 @step(u'And There is no received file')
 @raises(IOError)
